91589885985ddb052dc0aecd671343d0172a9372
Alobar <72378881+KingAlobar@users.noreply.github.com>
2025-04-13 21:54:22 -0700

    Add files via upload

---
+++ added1.c
@@ -0,0 +1,37 @@
+static Object SpawnObject(string type, vector position, vector orientation, float scale = 1.0)
+{
+    Object obj;
+    if (type.Contains(".p3d")) {
+        obj = GetGame().CreateStaticObjectUsingP3D(type, position, orientation, scale, false);
+    } else {        
+        obj = GetGame().CreateObjectEx(type, position, ECE_SETUP | ECE_CREATEPHYSICS | ECE_NOLIFETIME | ECE_NOPERSISTENCY_WORLD | ECE_NOPERSISTENCY_CHAR);
+    }
+
+    if (!obj) {
+        Error("Failed to create object " + type);
+        return null;
+    }
+
+    obj.SetPosition(position);
+    obj.SetOrientation(orientation);
+    obj.SetOrientation(obj.GetOrientation());
+    obj.SetScale(scale);
+    obj.Update();
+	obj.SetAffectPathgraph(true, false);
+	if (obj.CanAffectPathgraph()) {
+        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(GetGame().UpdatePathgraphRegionByObject, 100, false, obj);
+    } 
+
+    return obj;
+}
+
+// Paste anything below this line into the bottom of your 'void main()' function
+
+
+// Created Objects
+SpawnObject("TirePileLight", "4822.0 339.0 9611.0", "0.000000 0.000000 -0.000000", 1);
+
+
+// Uncomment if you want to export loot from newly added buildings
+// Position, Radius (increase if you have a larger map than Chernarus)
+// GetCEApi().ExportProxyData(Vector(7500, GetGame().SurfaceY(7500, 7500), 7500), 20000);
